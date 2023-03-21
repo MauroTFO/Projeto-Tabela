6d9ddb40881461aeb6b769ce9ea7bda59e9f7f54
MauroTFO <mevyness@gmail.com>
2023-03-21 11:40:32 -0300

    21/03

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+    $signos = [
+        'Áries' => 'Treteiro',
+        'Touro' => 'Come come',
+        'Gêmeos' => 'Instavel',
+        'Câncer' => 'Doido',
+        'Leão' => '',
+        'Virgem' => '',
+        'Escorpião' => '',
+        'Sargitário' => '',
+        'Capricórnio' => '',
+        'Aquário' => '',
+        'Peixes' => ''
+    ]
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,23 +21,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./index.css">
     <link rel="icon" href="https://icongr.am/material/card-account-details-outline.svg?size=128&color=ffffff">
-    <title>Tipes de LGTV</title>
+    <title>Tipes de YAGs</title>
 </head>
 <body>
     <form action="adicionar.php">   
     <input class="nome-input" name="nome" placeholder="Nome" type="text" >
-    <input placeholder="Signo" name="signo" type="tex">
+    <select name="signo" id="">
+        <?php foreach ($signos as $signo => $car): ?>
+            <option value="<?=$signo?>"><?=$signo?></option>
+        <?php endforeach ?>
+    </select>
     <input placeholder="Idade" name="idade" type="text">
     <input class="submit" type="submit">
+
+    <label class="input">
+      <input class="input__field" type="text" placeholder=" " />
+      <span class="input__label">Some Fancy Label</span>
+      
+    </label>
     </form>
     <table class="tabela">
         <?php $table = file('armario.csv') ?>
         <?php foreach($table as $row): ?>
             <?php list($nome, $signo, $idade) = explode(",", $row); ?>
+            <?php 
+                $chi = $signos[$signo]
+            ?>
             <tr>
             <td><a class="nomes" style="color:red; text-transform:uppercase">Nome: </a> <?= $nome ?></td> 
             <td><a class="nomes" style="color:red; text-transform:uppercase">Signo: </a> <?= $signo ?></td>
             <td><a class="nomes" style="color: red; text-transform:uppercase">Idade: </a><?= $idade ?></td>
+            <td><a class="nomes" style="color: red; text-transform:uppercase">Tipo de Yag: </a><?= $chi ?></td>
             </tr>
             <?php endforeach ?>
         </table>
